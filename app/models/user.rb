ecc647c49c12dcfb4383687ff812386801326206
happy4crazy <alan.m.odonnell@gmail.com>
2010-10-17 15:01:42 -0400

    User model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ActiveRecord::Base
+  # these are the only attributes that can be updated using #update_attributes
   attr_accessible :name, :email
+
+  validates :name, :presence => true, 
+                   :length => { :maximum => 50 }
+  validates :email, :presence => true, 
+                    :format => { :with => /\A[\w.\-]+@[\w.\-]+\.\w+\z/ },
+                    :uniqueness => { :case_sensitive => false }
 end
