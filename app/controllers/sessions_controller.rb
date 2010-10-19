79ebc3e10e2304fb1d400a3b8637356a0275ef21
happy4crazy <alan.m.odonnell@gmail.com>
2010-10-19 13:28:28 -0400

    Handle signin failure

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,18 @@ class SessionsController < ApplicationController
   end
 
   def create
-    
+    user = User.authenticate(params[:session][:email], 
+                             params[:session][:password])
+
+    if user.nil?
+      @title = "Sign in"
+      flash.now[:error] = "Something went wrong!"
+      render :new
+    else
+      cookies[:user_id] = user.id
+      flash[:success] = "Welcome back!"
+      redirect_to user
+    end
   end
 
   def destroy
--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -15,4 +15,28 @@ describe SessionsController do
     end
   end
 
+  describe "POST 'create'" do
+    context "failure" do
+      before(:each) do
+        @session = { :email => "", :password => "" }
+      end
+
+      it "should have the right title" do
+        post :create, :session => @session
+        response.should have_selector('title',
+                                      :content => 'Sign in')
+      end
+
+      it "should re-render the new page" do
+        post :create, :session => @session
+        response.should render_template('new')
+      end
+
+      it "should display an error message" do
+        post :create, :session => @session
+        flash.now[:error].should =~ /wrong/i
+      end
+    end
+  end
+
 end
